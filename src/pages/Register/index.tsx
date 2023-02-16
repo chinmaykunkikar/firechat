adb26b8dcb2caac09d6ddffdf236f61008f262ec
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-16 20:05:26 +0530

    feat(pages): Add logic to Register form

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,10 +1,51 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
+import { auth, storage } from "../../firebase";
+
 export default function Register() {
+  const [error, setError] = useState<boolean>(false);
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    const displayName: string = e.target[0].value;
+    const email: string = e.target[1].value;
+    const password: string = e.target[2].value;
+    const avatar: Blob = e.target[3].files[0];
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, avatar);
+      uploadTask.on(
+        "state_changed",
+        (error: any) => setError(true),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            async (downloadURL: string) => {
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+            }
+          );
+        }
+      );
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-8">
       <div className="card flex w-1/3 flex-col rounded-3xl bg-base-300 p-8">
         <div className="mx-auto mb-2 text-3xl font-bold">Fire Chat</div>
         <div className="mx-auto mb-4">Create a new account</div>
-        <form className="flex flex-col items-center gap-4">
+        <form
+          className="flex flex-col items-center gap-4"
+          onSubmit={handleSubmit}
+        >
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">What is your name?</span>
@@ -45,6 +86,7 @@ export default function Register() {
             />
           </div>
           <button className="btn-primary btn-wide btn mt-2">Sign up</button>
+          {error && <div className="text-error">Something went wrong.</div>}
         </form>
         <p className="mt-4 text-center">
           Already have an account?{" "}

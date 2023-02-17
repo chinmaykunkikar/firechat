4bb90c9fde829c19cc3836873583e85ad78dcb9a
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-18 00:09:36 +0530

    feat(Login): Authenticate user and redirect to the chats page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
+import { auth } from "@/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-8">
       <div className="card flex w-1/3 flex-col rounded-3xl bg-base-300 p-8">
         <div className="mx-auto mb-2 text-3xl font-bold">Fire Chat</div>
         <div className="mx-auto mb-4">Log into your account</div>
-        <form className="flex flex-col items-center gap-4">
+        <form
+          className="flex flex-col items-center gap-4"
+          onSubmit={handleSubmit}
+        >
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Enter your email address</span>
@@ -30,6 +51,7 @@ export default function Login() {
             />
           </div>
           <button className="btn-primary btn-wide btn mt-2">Login</button>
+          {error && <div className="text-error">Something went wrong.</div>}
         </form>
         <p className="mt-4 text-center">
           Don't have an account?{" "}

49b7c944903400f41a9679c4010dcaa7ce1cbfe7
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-17 23:46:07 +0530

    feat(Navbar): Add signout logic; set name and avatar from `currentUser`

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,27 @@
+import { AuthContext } from "@contexts/AuthContext";
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
+import { useContext } from "react";
 
 export default function Navbar() {
+  const { currentUser }: any = useContext(AuthContext);
   return (
     <div className="flex h-16 select-none items-center justify-between bg-secondary px-4">
       <span className="text-xl font-semibold">Fire Chat</span>
       <div className="flex items-center justify-center gap-2">
         <div>
-          <div className="placeholder avatar">
+          <div className="avatar">
             <div className="w-8 rounded-full bg-neutral-focus text-neutral-content">
-              <span className="text-sm font-semibold">ck</span>
+              <img src={currentUser.photoURL} alt={currentUser.displayName} />
             </div>
           </div>
         </div>
-        <div className="font-semibold">Chinmay Kunkikar</div>
-        <button className="btn-square btn-ghost btn-xs btn">
+        <div className="font-semibold">{currentUser.displayName}</div>
+        <button
+          className="btn-ghost btn-square btn-xs btn"
+          onClick={() => signOut(auth)}
+        >
           <ArrowRightOnRectangleIcon />
         </button>
       </div>

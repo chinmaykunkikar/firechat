b3ec71abf280eeaf1a2e2dc8d7818d6e89082713
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-19 01:52:12 +0530

    chore(firechat): Wrap the app with `ChatContextProvider`

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { AuthContextProvider } from "@contexts/AuthContext";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { AuthContextProvider } from "@contexts/AuthContext";
+import { ChatContextProvider } from "./contexts/ChatContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChatContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChatContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );

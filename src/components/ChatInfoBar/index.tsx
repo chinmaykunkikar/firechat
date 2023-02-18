7ef4b030595b2ce4b30b2600164f6c8818c92c6e
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-19 01:53:47 +0530

    feat(Chat): Show username of the active chat on `ChatInfoBar`

--- src/components/ChatInfoBar/index.tsx
+++ src/components/ChatInfoBar/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { ChatContext } from "@/contexts/ChatContext";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 
 export default function ChatInfoBar() {
+  const { data }: any = useContext(ChatContext);
+
   return (
     <div>
       <div className="flex h-16 select-none items-center justify-between bg-secondary-focus px-6">
-        <div className="text-lg font-semibold">Ayush Swamy</div>
+        <div className="text-lg font-semibold">{data.user?.displayName}</div>
         <button className="btn-ghost btn-square btn-sm btn">
           <EllipsisVerticalIcon />
         </button>

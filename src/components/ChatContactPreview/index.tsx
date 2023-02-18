32b506a271555d4c525d8e86c51e23f5169c50b3
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-18 22:30:26 +0530

    feat(Search): Add logic to search users from db and create a chat document onClick

--- src/components/ChatContactPreview/index.tsx
+++ src/components/ChatContactPreview/index.tsx
@@ -1,15 +1,25 @@
-export default function ChatContactPreview() {
+import { DocumentData } from "firebase/firestore";
+
+type ChatContactPreviewProps = {
+  handleSearchSelect?: React.MouseEventHandler<HTMLDivElement> | undefined;
+  user?: DocumentData;
+};
+
+export default function ChatContactPreview({
+  user,
+  handleSearchSelect: handleSelect,
+}: ChatContactPreviewProps) {
   return (
-    <div className="flex gap-3">
+    <div className="flex gap-3" onClick={handleSelect}>
       <div>
-        <div className="placeholder avatar">
+        <div className="avatar">
           <div className="w-12 rounded-full bg-neutral-focus font-semibold text-neutral-content">
-            <span>as</span>
+            <img src={user?.photoURL} alt={user?.displayName} />
           </div>
         </div>
       </div>
       <div className="flex flex-col">
-        <div className="font-semibold">Ayush Swamy</div>
+        <div className="font-semibold">{user?.displayName}</div>
         <div className="text-neutral-content">Hello</div>
       </div>
     </div>
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,108 @@
+import { AuthContext } from "@/contexts/AuthContext";
+import { db } from "@/firebase";
+import {
+  collection,
+  doc,
+  DocumentData,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useContext, useState } from "react";
+import ChatContactPreview from "../ChatContactPreview";
+
 export default function Search() {
+  const [username, setUsername] = useState<string>("");
+  const [user, setUser] = useState<DocumentData>();
+  const [error, setError] = useState<boolean>(false);
+
+  const { currentUser }: any = useContext(AuthContext);
+
+  async function handleSearch() {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(true);
+    }
+  }
+
+  function handleKey(e: any) {
+    e.code === "Enter" && handleSearch();
+  }
+
+  async function handleSelect() {
+    const combinedId =
+      user &&
+      (currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid);
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        await updateDoc(
+          doc(db, "userChats", currentUser.uid),
+          user && {
+            [combinedId + ".userInfo"]: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          }
+        );
+
+        await updateDoc(doc(db, "userChats", user?.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setError(true);
+    }
+
+    setUser(undefined);
+    setUsername("");
+  }
+
   return (
     <div>
       <input
         type="text"
-        placeholder="Search&#8230;"
+        placeholder="Search for a user&#8230;"
+        onKeyDown={handleKey}
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
         className="input-ghost input input-sm mt-2 w-full rounded-none focus:bg-transparent focus:outline-none"
       />
+      {error && (
+        <span className="text-sm font-semibold text-warning">
+          User not found. Try searching for the exact name.
+        </span>
+      )}
+      <div className="px-3">
+        {user && (
+          <ChatContactPreview user={user} handleSearchSelect={handleSelect} />
+        )}
+      </div>
       <div className="divider m-0"></div>
     </div>
   );

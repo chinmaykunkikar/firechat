f820b200aecb6a2a34d457c603bf36ae033f927c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-20 02:31:29 +0530

    chore(contexts): Delete console log statement

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -10,7 +10,6 @@ export const AuthContextProvider = ({ children }: any) => {
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
-      console.log(user);
     });
 
     return () => {

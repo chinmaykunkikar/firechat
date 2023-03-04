8b0b06f3a96c9665e86e042c565c14aa00fd7556
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-05 01:57:42 +0530

    feat(Chat): Add an empty state message if chat is empty

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,6 +1,7 @@
 import { ChatContext } from "@/contexts/ChatContext";
 import { db } from "@/firebase";
 import MessageBubble from "@components/MessageBubble";
+import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/outline";
 import { doc, onSnapshot } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 
@@ -20,9 +21,16 @@ export default function Conversation() {
 
   return (
     <div className="grow overflow-y-auto p-4">
-      {messages.map((message: any) => (
-        <MessageBubble message={message} key={message.id} />
-      ))}
+      {messages.length !== 0 ? (
+        messages.map((message: any) => (
+          <MessageBubble message={message} key={message.id} />
+        ))
+      ) : (
+        <div className="flex h-full flex-col items-center justify-center">
+          <ChatBubbleLeftEllipsisIcon className="h-40 w-40 stroke-accent" />
+          <p className="select-none text-lg">Strike a conversation!</p>
+        </div>
+      )}
     </div>
   );
 }

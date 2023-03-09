4fa240d427e7953521e9dca27b84be4f0a34818d
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-10 02:45:57 +0530

    style(firechat): Change daisyUI theme to `business`
    - Increase main chat area

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@components/Sidebar";
 export default function Chat() {
   return (
     <div className="home flex h-screen items-center justify-center">
-      <div className="card flex h-5/6 w-5/6 flex-row overflow-hidden shadow-xl">
+      <div className="card flex h-[90%] w-[90%] flex-row overflow-hidden shadow-xl">
         <Sidebar />
         <ChatMain />
       </div>

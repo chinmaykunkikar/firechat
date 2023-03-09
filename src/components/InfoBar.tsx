92346f2dbe3375a343e33c5071a855305b4d6301
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-10 04:16:22 +0530

    style(Chat): Show user avatar on the InfoBar as well

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ChatContext } from "@/contexts/ChatContext";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+import Avvvatars from "avvvatars-react";
 
 export default function InfoBar() {
   const { data }: any = useContext(ChatContext);
@@ -8,7 +9,10 @@ export default function InfoBar() {
   return (
     <div>
       <div className="flex h-16 select-none items-center justify-between bg-secondary-focus px-6">
-        <div className="text-lg font-semibold">{data.user?.displayName}</div>
+        <div className="flex items-center gap-4">
+          <Avvvatars value={data?.user?.uid} style="shape" />
+          <div className="text-lg font-semibold">{data.user?.displayName}</div>
+        </div>
         <button className="btn-ghost btn-square btn-sm btn">
           <EllipsisVerticalIcon />
         </button>

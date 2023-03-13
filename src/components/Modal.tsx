eed9a2484c14ff4f026ee7e1bc967fb189d56cee
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-13 22:06:17 +0530

    feat(components): Add a reuseable modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import cn from "classnames";
+type Props = {
+  children: React.ReactNode;
+  open: boolean;
+  onClose(): void;
+};
+
+const Modal = ({ children, open, onClose }: Props) => {
+  const ref = useRef(null);
+
+  const modalClass = cn({
+    "modal modal-bottom sm:modal-middle": true,
+    "modal-open": open,
+  });
+  return (
+    <div className={modalClass}>
+      <div className="modal-box" ref={ref}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

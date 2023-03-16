be2f1e5476bf41bb812a723d4a1f682c58c8d325
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-17 01:53:22 +0530

    style(Chat): Make chatbox full screen

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,8 +3,8 @@ import Sidebar from "@components/Sidebar";
 
 export default function Chat() {
   return (
-    <div className="home flex h-screen items-center justify-center">
-      <div className="card flex h-[90%] w-[90%] flex-row overflow-hidden shadow-xl">
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex h-screen w-screen flex-row overflow-hidden">
         <Sidebar />
         <ChatMain />
       </div>

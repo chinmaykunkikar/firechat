22c2dfb7dd6004611d5858156788c9ac190d3157
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-03-21 03:15:49 +0530

    chore(utils): Add a `generateAvatar()` helper function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -85,3 +85,30 @@ export function getJoiningDate(utcDate: Date) {
     year: "numeric",
   }).format(utcDate);
 }
+
+export const generateAvatar = () => {
+  const generateDiceBearAvataaars = (seed: any) =>
+    `https://avatars.dicebear.com/api/avataaars/${seed}.svg`;
+
+  const generateDiceBearBottts = (seed: any) =>
+    `https://avatars.dicebear.com/api/bottts/${seed}.svg`;
+
+  const generateDiceBearGridy = (seed: any) =>
+    `https://avatars.dicebear.com/api/gridy/${seed}.svg`;
+
+  const data = [];
+
+  for (let i = 0; i < 2; i++) {
+    const res = generateDiceBearAvataaars(Math.random());
+    data.push(res);
+  }
+  for (let i = 0; i < 2; i++) {
+    const res = generateDiceBearBottts(Math.random());
+    data.push(res);
+  }
+  for (let i = 0; i < 2; i++) {
+    const res = generateDiceBearGridy(Math.random());
+    data.push(res);
+  }
+  return data;
+};
